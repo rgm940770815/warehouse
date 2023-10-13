d2a89537409c59acfc94f8e1a4f242643580e9b7
冉光明 <940770815@qq.com>
2023-10-13 20:39:58 +0800

    添加自定义异常处理类

---
+++ src/main/java/com/yeqifu/bus/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.yeqifu.bus.exception;
+
+/**
+ * 自定义异常类
+ */
+public class CustomException extends RuntimeException {
+
+    public CustomException(String msg){
+        super(msg);
+    }
+}
